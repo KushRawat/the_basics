0389dc753a95335d0b908797a90e859d3dc03b7b
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-15 18:28:12 +0530

    edit

--- basics.py
+++ basics.py
@@ -1,7 +1,1 @@
-daily_temperatures = (21,12,2,4)
-students_grades = {"Jim": 12, "Lahey": 13}
-
-mySum = sum(students_grades.values())
-length = len(students_grades)
-mean = mySum/length
-print(mean)
+monday_temperatures = ["hello",12,234]
