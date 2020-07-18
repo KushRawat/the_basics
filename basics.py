73f8f46405c0c6ab91a881158c487f7b2f7ed1dd
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-18 19:07:33 +0530

    edit

--- basics.py
+++ basics.py
@@ -1,11 +1,12 @@
-def mean(myList):
-    the_mean = sum(myList) / len(myList)
-    #return the_mean
-    print(the_mean)
-    #return None
+def mean(value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)   
+    
+    return the_mean
 
-print(mean([3,4,5]))
-print(type(mean))
-#print(type(myList))
-print(type(len))
-print(type(sum))
+dictionary = {"kush" : 12, "rawat" : 23}
+temperatures = [12,243,65]
+print(mean(dictionary))
+print(mean(temperatures))
