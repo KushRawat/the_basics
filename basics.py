6fb0bbbd876dc56d8e7c99fd292dafda4d3aab7f
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-18 17:39:56 +0530

    edit

--- basics.py
+++ basics.py
@@ -1,6 +1,8 @@
 def mean(myList):
     the_mean = sum(myList) / len(myList)
-    return the_mean
+    #return the_mean
+    print(the_mean)
+    #return None
 
 print(mean([3,4,5]))
 print(type(mean))
