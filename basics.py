0729683a4e3798346de427e1aea4312e857d57e3
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-19 17:14:53 +0530

    user input

--- basics.py
+++ basics.py
@@ -1,12 +1,8 @@
-def mean(value):
-    if type(value) == dict:
-        the_mean = sum(value.values()) / len(value)
-    else:
-        the_mean = sum(value) / len(value)   
-    
-    return the_mean
+def condition(x):
+    if x > 7:
+        return "WArm"
+    else: 
+        return "cold"
 
-dictionary = {"kush" : 12, "rawat" : 23}
-temperatures = [12,243,65]
-print(mean(dictionary))
-print(mean(temperatures))
+user_input = int(input("Enter temperature:"))
+print(condition(user_input))
