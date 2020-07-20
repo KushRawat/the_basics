6ecc47015762a0f51147b37e81eca144ce3e209c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-20 17:33:13 +0530

    another formatting method

--- basics.py
+++ basics.py
@@ -2,7 +2,8 @@ name = input("Enter your name:")
 surname = input("Enter your surname:")
 when = "today"
 
-message = "Hello %s %s whats up %s" %(name, surname, when)
+#message = "Hello %s %s whats up %s" %(name, surname, when)
 #message = f"Hello {name} {surname}, whats up {when}"
+message = "Hello {} {}, whats up {}".format(name, surname, name)
 print(message)
 
