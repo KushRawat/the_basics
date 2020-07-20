c0a06b5303aeafb54f97387626570f0f6482baa4
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-20 18:37:08 +0530

    string formatting dict loops

--- basics.py
+++ basics.py
@@ -1,4 +1,4 @@
 monday_temperatures = {"my" : 12, "hi" : 23, "die" : 45}
 for grades in monday_temperatures.items(): #.keys(),.values()
-    print(grades)
+    print ("{} has a number {}".format(grades[0],grades[1]))
 
