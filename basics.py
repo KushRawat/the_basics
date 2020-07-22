c1f6f1cc4db9fea97c8d7484b14de53b02fbafba
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-22 23:28:34 +0530

    edit

--- basics.py
+++ basics.py
@@ -2,3 +2,5 @@ monday_temperatures = {"my" : 12, "hi" : 23, "die" : 45}
 for a,b in monday_temperatures.items(): #.keys(),.values()
     print ("{} has a number {}".format(a,b))
     #print(a,b)
+    #print(a,b)
+    
