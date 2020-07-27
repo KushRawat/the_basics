ab1784c1d3791cd4f7afcee1f37b6d54752af80c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-07-28 03:21:19 +0530

    sentenceMaker.py

--- basics.py
+++ basics.py
@@ -1,7 +1,17 @@
+def sentenceMaker(phrase):
+    interrogatives = ("how", "when", "why" )
+    capitalized = phrase.capitalize()
+    if phrase.startswith(interrogatives):
+        return f"{capitalized}?"
+    else:
+        return f"{capitalized}."
+
+result = []
 while True:
-    username = input("Enter username: ")
-    if username == "Kush":
+    user_input = input("Say something: ")
+    if user_input == "/end":
         break
     else:
-        continue
+        result.append(sentenceMaker(user_input))
 
+print(" ".join(result))
