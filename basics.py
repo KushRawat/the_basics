e7784517a1377ee9a07b9024ecc14703ddc6fb41
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-02 02:11:11 +0530

    default and non default parameter

--- basics.py
+++ basics.py
@@ -1,8 +1,10 @@
-def area(a, b):
+def area(a, b= 7): # defualt parameter
     return a * b
 
-print(area(a = 4, b = 3)) # keyword arguments
-print(area(b = 4, a = 7)) # KEYWORD ARGUMENTS position doesnt matter 
+print(area(a = 4)) # keyword arguments , no need to assign b 
+print(area(5)) # non KEYWORD ARGUMENTS , no need to assign a or b 
+print(area(a = 4, b = 10)) # b can be assigned different value
+print(area(5, 7)) # non KEYWORD ARGUMENTS
 
-print(area(a, b)) # non-keyword arguments, positional argumentssince the way these arguments behave for a function is based on position
- 
+def area(a = 5, b): # a defualt parameter cannot be before a non-default parameter
+    return a * b
