26f90ed053d6b46306e3d30b4498bc80d1ab600f
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-02 03:22:24 +0530

    **kwargs (for keyword arguments)

--- basics.py
+++ basics.py
@@ -1,8 +1,8 @@
-def mean(*args): # args is used as a parameter with the power of processing as many arguments as we want, and returns type of tuple
-    print (args)
-    print (type(args))
-    print(sum(args))
-
+def mean(**kwargs): # this is used when key words arguments need to be used ,args is used as a parameter with the power of processing as many arguments as we want, and returns type of tuple
+    #print (args)
+    #print (type(args))
+    #print(sum(args))
+    return kwargs
 #print(mean(1, 2, 3, 4, "a"))
-print(mean(1, 2, 3, 4))
-print(mean(1, 2, x = 3, 4)) # keyword argumetn will not work heree
+#print(mean(1, 2, 3, 4)) # willl not work
+print(mean(a = 1, b = 2, x = 3, y = 4)) 
