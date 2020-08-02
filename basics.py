a095b651197f02ed1583a0b0e9e8d28f8dd30db1
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-03 02:01:50 +0530

    overwriting a file

--- basics.py
+++ basics.py
@@ -1,4 +1,2 @@
-
-with open("vegetables.txt", "w") as myfile: # making a new file vegetables.txt and performing write mode with "w"
-    myfile.write("Tomato") # using .write to write in the file 
-# with the ending of this block the file closes on its own
+with open("fruits.txt", "w") as myfile:
+    myfile.write("Tomato\nCucumber\nOnion")
