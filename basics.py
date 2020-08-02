f020e52b1f42b08c4cad00d7c5325255c565a2ac
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-02 23:59:04 +0530

    kwargs returns dictionary

--- basics.py
+++ basics.py
@@ -1,4 +1,6 @@
 def find_sum(**kwargs):
-    return sum(kwargs.values()) # method of calling value from keyword argument in case of **kwargs parameter in a function
+    return (kwargs) # method of calling value from keyword argument in case of **kwargs parameter in a function
 
 print(find_sum(x = 9))
+
+#returns a dictionary
