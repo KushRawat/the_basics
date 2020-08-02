8172192b7393a609929cb0526a03e43604651d4e
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-08-03 00:29:13 +0530

    return max(kwargs, key = kwargs.get)

--- basics.py
+++ basics.py
@@ -1,4 +1,4 @@
 def find_winner(**kwargs):
-    return max(kwargs, key = kwargs.get)
+    return max(kwargs, key = kwargs.get) #kwargs.get Returns the value for key if key is in the dictionary, else default.
  
 print(find_winner(Andy = 17, Marry = 19, Sim = 45, Kae = 34))
