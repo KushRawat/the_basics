7859d3a485fb6fd9394b7f61c0d15db1f367ecbb
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-09-13 15:18:14 +0530

    appending text to an existing file

--- basics.py
+++ basics.py
@@ -1,2 +1,6 @@
-with open("fruits.txt", "a") as myfile:     # unlike "w", "x" does not overwrites file
-    myfile.write("Okra")
+with open("fruits.txt", "a+") as myfile:    # usinf a+ to append and read at the same time otherwise we wont be able to read the file     # unlike "w", "x" does not overwrites file
+    myfile.write("\nOkra") # to append on anew line
+    myfile.seek(0)   # to bring cursor back to 0/top
+    content = myfile.read()
+
+print(content)
