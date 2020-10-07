8670d21920dfaf052d3ea5c9f7348265a4c66b2c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-07 21:33:51 +0530

    sys.builtin_module_names

--- basics.py
+++ basics.py
@@ -1,6 +1,6 @@
-with open("fruits.txt", "a+") as myfile:    # usinf a+ to append and read at the same time otherwise we wont be able to read the file     # unlike "w", "x" does not overwrites file
-    myfile.write("\nOkra") # to append on anew line
-    myfile.seek(0)   # to bring cursor back to 0/top
-    content = myfile.read()
+# using sys.builtin_module_names
 
-print(content)
+while True:
+    with open("vegetables.txt") as file:
+        print(file.read())
+
