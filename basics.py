5b3e18f29d21fa8914845e1cd02584a9b582ed91
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-12 16:19:51 +0530

    os.path.exists

--- basics.py
+++ basics.py
@@ -1,9 +1,11 @@
-# using sys.builtin_module_names
-
 import time
+import os
 
 while True:
-    with open("vegetables.txt") as file:
-        print(file.read())
-        time.sleep(10)
+    if os.path.exists("vegetables.txt"):
+        with open("vegetables.txt") as file:
+            print(file.read())
+    else:
+        print("File does not exist.")
+    time.sleep(3)
 
