bf7be8240a2bdee4130dfbd0c96751b5f61889a9
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-13 20:07:54 +0530

    more about libraries and pandas

--- basics.py
+++ basics.py
@@ -1,10 +1,11 @@
-import time
-import os
+import time     # BUILT IN LIBRARY (WRITTEN IN C LANG)
+import os       # STANDARD LIBRARIES # USE SYS.PREFIX TO FIND (WRIITEN IN C AND PYTHON BOTH)
 import pandas # third party library, it's written in python itself
 
 while True:
     if os.path.exists("temps_today.csv"):
-        data = pandas.read_csv("temps_today.csv")    # method to access csv through pandas
+        # CREATING A DATA FRAME
+        data = pandas.read_csv("temps_today.csv")    # method to access csv through pandas 
         print(data.mean()["st1"])      # using mean only for st1
     else:
         print("File does not exist.")
