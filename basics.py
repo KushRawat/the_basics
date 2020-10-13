d178b1065904abb584fa6bc8051d2c0268aa2757
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-10-13 19:59:58 +0530

    using pandas

--- basics.py
+++ basics.py
@@ -1,11 +1,13 @@
 import time
 import os
+import pandas # third party library, it's written in python itself
 
 while True:
-    if os.path.exists("vegetables.txt"):
-        with open("vegetables.txt") as file:
-            print(file.read())
+    if os.path.exists("temps_today.csv"):
+        data = pandas.read_csv("temps_today.csv")    # method to access csv through pandas
+        print(data.mean())      # using mean method
     else:
         print("File does not exist.")
     time.sleep(3)
 
+#pandas is a package, a bunch of modules, but can be still referred to as a library
