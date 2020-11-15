b0d324b082a9a5f6c44ab03c787be28148e0af6c
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-11-15 16:59:02 +0530

    indexing, slicing, iterating numpy arrays

--- app2.py
+++ app2.py
@@ -1,4 +1,8 @@
 import cv2
-img = cv2.imread("smallgray.png", 1)   # 0 is greyscale, # use 1 for bgr(numpy array)
-print(img)
-cv2.imwrite("newsmallgray.png", img)  # making images from numpy arrays
+img = cv2.imread("smallgray.png", 0)   # 0 is greyscale, # use 1 for bgr(numpy array)
+# print(img)
+# cv2.imwrite("newsmallgray.png", img)  # making images from numpy arrays
+print(img[0:2,2:4])
+
+for i in img.flat:          # prints the elements one by one
+    print(i)
